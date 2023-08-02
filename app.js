2cbd705a999990114e99c677deae5a22046d2642
An-Wilson <ta102779@gmail.com>
2023-08-02 10:13:30 +0800

    feat: project init & add mongoose

---
+++ app.js
@@ -0,0 +1,34 @@
+// includes express
+const express = require('express')
+const app = express()
+const port = 3000
+
+// includes mongoose
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+// includes handlebars
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`The Express server is listening on http://localhost:${port}`)
+})
