4dac47c2249f627aae218870f83d08f20223f608
An-Wilson <ta102779@gmail.com>
2023-08-02 11:29:23 +0800

    feat: add shortenUrl function

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const Url = require('./models/urls')
+const shortenUrl = require('./shortenUrl')
+const shortLength = 5 // 可調整縮短網址的亂碼字數
 
 // includes mongoose
 const mongoose = require('mongoose')
@@ -24,13 +27,19 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.urlencoded({ extended: true }))
+
 // setting routes
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.post('/urls', (req, res) => {
-  
+  const inputUrl = req.body.inputUrl
+  Url.findOne({ originalUrl: inputUrl })
+    .then(data => data ? data : Url.create({ originalUrl: inputUrl, shortUrl: shortenUrl(shortLength) }))
+    .then(data => res.render('index', { shortUrl: data.shortUrl }))
+    .catch(err => console.error(err))
 })
 
 app.listen(port, () => {
